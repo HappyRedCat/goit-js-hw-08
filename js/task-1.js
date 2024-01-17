11b47b70f31e5d588c85818fe604d24856d9a6f1
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-01-17 19:53:40 +0100

    up

--- js/task-1.js
+++ js/task-1.js
@@ -2,3 +2,11 @@ const a = 45;
 const b = 35;
 
 console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
