886813822dd45c0746141f7658dfe35a17ad958b
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-01-17 19:54:48 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,14 @@
 const a = 45;
 const b = 35;
 
-console.log(a + b);
-console.log(a + b);
-console.log(a + b);
+const c = 35;
+const u = 35;
+const o = 35;
+const h = 35;
+
+console.log(a + b + c + b);
+console.log(a + b - u);
+console.log(a + b + o);
 console.log(a + b);
 console.log(a + b);
 console.log(a + b);
